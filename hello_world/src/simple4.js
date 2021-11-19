4d4ad466e5f8400d700ed580e60f17e2a2d24335
ABlued <53801395+ABlued@users.noreply.github.com>
2021-11-20 08:15:19 +0900

    바벨로 jsx문법을 변환해보기

---
+++ hello_world/src/simple4.js
@@ -0,0 +1,30 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return React.createElement(
+    'button',
+    {
+      onClick: () => setLiked(!liked),
+    },
+    text
+  );
+}
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <LikeButton />
+      <div style={{ marginTop: 20 }}>
+        <span>현재 카운트</span>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+      </div>
+    </div>
+  );
+}
+
+const domContainer = document.querySelector('#react-root');
+ReactDOM.render(React.createElement(Container), domContainer);
+// 바벨로 jsx 문법 변환해주는 명령어
+// npx babel --watch src --out-dir ./build , --presets @babel/preset-react
